6b03cd855f6d7ad62ed6389a09611b1493a084c4
Matthías Ásgeirsson <matthias.asgeirsson@gmail.com>
2015-05-08 16:09:13 +0000

    Minimal version of face detection on live feed from webcam with opencv. Image scaled down 50% at start and mirrored before viewing.

--- minimal_cv_facedetect.py
+++ minimal_cv_facedetect.py
@@ -6,15 +6,15 @@ import os
 
 cap = cv2.VideoCapture(0)
 cascate_root = '/opt/local/share/OpenCV/haarcascades/'
-face_cascade = cv2.CascadeClassifier(os.path.join(cascate_root, 'haarcascade_frontalface_alt.xml'))
-green = (0, 255, 0)
+cascade_name = 'haarcascade_frontalface_alt.xml'
+face_cascade = cv2.CascadeClassifier(os.path.join(cascate_root, cascade_name))
 
 while(True):
     ret, frame = cap.read()
     frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
-    faces = face_cascade.detectMultiScale(frame, 1.3, 5)
+    faces = face_cascade.detectMultiScale(frame, 1.2, 5)
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), green, 1)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
     frame = cv2.flip(frame, 1)
     cv2.imshow('frame', frame)
     key = cv2.waitKey(1)
